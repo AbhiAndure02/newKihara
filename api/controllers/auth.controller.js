3d4f64d1489e9431ba6f7956d883f06eec23b54b
AbhiAndure02 <abhiandure123@gmail.com>
2024-10-04 16:31:39 +0530

    done

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -51,8 +51,7 @@ export const signin = async (req, res, next) => {
       }
       const token = jwt.sign(
         { id: validUser._id, isAdmin: validUser.isAdmin },
-        process.env.JWT_SECRET
-      );
+        "kiharas"      );
   
       const { password: pass, ...rest } = validUser._doc;
   
